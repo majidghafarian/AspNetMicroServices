34e83b10a45e10b547cf813fcf856f81ff2f3f32
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-03-21 12:39:41 +0330

    catalog dev

---
+++ src/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.OpenApi.Validations;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var Client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = Client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products =database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextseed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
