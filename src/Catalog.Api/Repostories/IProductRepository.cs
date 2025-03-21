e49b0296e589e17c6bc943c83d6813089bd5f648
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-03-21 13:14:33 +0330

    repo category build

---
+++ src/Catalog.Api/Repostories/IProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Api.Entities;
+
+namespace Catalog.Api.Repostories
+{
+    public interface IProductRepository
+    {
+        // متد برای افزودن یک محصول جدید
+        Task AddProductAsync(Product product);
+
+        // متد برای دریافت همه محصولات
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+
+        // متد برای دریافت محصول بر اساس شناسه (ID)
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<IEnumerable< Product>> GetProductByNameAsync(string Name);
+        Task<Product> GetProductByCategoryAsync(string CategoryName);
+
+        // متد برای به‌روزرسانی محصول موجود
+        Task <bool>UpdateProductAsync(  Product updatedProduct);
+
+        // متد برای حذف محصول
+        Task<bool> DeleteProductAsync(Guid id);
+    }
+}
---
+++ src/Catalog.Api/Repostories/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Xml.Linq;
+
+namespace Catalog.Api.Repostories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+
+        public async Task<bool> DeleteProductAsync(Guid id)
+        {
+            var filter = Builders<Product>.Filter.Eq(p => p.Id, id);  // ایجاد فیلتر برای پیدا کردن محصول با شناسه موردنظر
+            var result = await _context.Products.DeleteOneAsync(filter);  // استفاده از فیلتر برای حذف محصول
+            return result.IsAcknowledged&& result.DeletedCount  > 0;  // بررسی اینکه آیا محصول حذف شده است یا خیر
+        }
+
+
+
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.Find(_ => true).ToListAsync();
+        }
+
+
+        public Task<Product> GetProductByCategoryAsync(string CategoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProductByIdAsync(Guid id)
+        {
+            return await _context
+                .Products
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByNameAsync(string Name)
+        {
+            var filter = Builders<Product>.Filter.Regex(p => p.Name, new BsonRegularExpression(Name, "i"));  // "i" برای جستجوی غیرحساس به حروف
+            return await _context.Products
+                                  .Find(filter)
+                                  .ToListAsync();
+        }
+
+
+
+        public async Task<bool> UpdateProductAsync( Product updatedProduct)
+        {
+            var result = await _context
+                .Products
+                .ReplaceOneAsync(p => p.Id == updatedProduct.Id, updatedProduct);
+            return result
+                .IsAcknowledged 
+                && result.ModifiedCount > 0;
+        }
+
+    }
+}
