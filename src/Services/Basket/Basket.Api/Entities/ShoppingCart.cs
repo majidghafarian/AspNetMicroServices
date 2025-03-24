2a7917dcaa552eb4895802d13bfe69791e066e0a
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-03-24 20:18:32 +0330

    dev basket

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string _username)
+        {
+            UserName = _username;
+        }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price+item.Quantity;
+                }
+                return totalprice;
+            }
+
+        }
+    }
+}
--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(option =>
+    option.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString")
+);
+
+
 
 var app = builder.Build();
 
