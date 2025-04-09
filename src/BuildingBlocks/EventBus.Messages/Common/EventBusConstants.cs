d25c10640e90e1815780a1e7ea917f578673857f
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-04-09 20:33:44 +0330

    RabitMq Dev

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBusConstants
+    {
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+        //public const string BasketCheckoutExchange = "basketcheckout-exchange";
+        //public const string BasketCheckoutRoutingKey = "basketcheckout-routingkey";
+        //public const string BasketCheckoutEmailQueue = "basketcheckout-email-queue";
+        //public const string BasketCheckoutEmailExchange = "basketcheckout-email-exchange";
+        //public const string BasketCheckoutEmailRoutingKey = "basketcheckout-email-routingkey";
+        //public const string BasketCheckoutPaymentQueue = "basketcheckout-payment-queue";
+        //public const string BasketCheckoutPaymentExchange = "basketcheckout-payment-exchange";
+        //public const string BasketCheckoutPaymentRoutingKey = "basketcheckout-payment-routingkey";
+    }
+}
---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,20 @@
+﻿
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id= Guid.NewGuid();
+            CreateDate = DateTime.UtcNow;
+        }
+        public IntegrationBaseEvent(Guid id, DateTime createdate)
+        {
+            Id = id;
+            CreateDate = createdate;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime CreateDate { get; private set; }
+    }
+}
