0834ad7ab57870aeb8be1b78453895a1f66c2002
Majid Ghafarian <m.ghafarian15@gmail.com>
2025-04-12 21:08:59 +0330

    dev RabbitMq

---
+++ src/Services/Ordering.Api/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+ using Ordering.Application.Features.Commands.CheckOutOrder;
+
+namespace Ordering.Api.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly MediatR.IMediator _mediator;
+        private readonly IMapper _mapper;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+        public BasketCheckoutConsumer(ILogger<BasketCheckoutConsumer> logger,IMapper mapper, MediatR.IMediator mediator)
+        {
+            _mediator=mediator;
+            _mapper=mapper;
+            _logger=logger;
+        }
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+           var Conmmand=_mapper.Map<CheckOutOrderCommand>(context.Message);
+            Conmmand.UserName = context.Message.UserName;
+            _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order for UserName: {newUserName}", Conmmand.UserName);
+           var res= await _mediator.Send(Conmmand);
+        }
+    }
+}
--- src/Services/Ordering.Api/Ordering.Api/Program.cs
+++ src/Services/Ordering.Api/Ordering.Api/Program.cs
@@ -1,4 +1,7 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+using Ordering.Api.EventBusConsumer;
 using Ordering.Api.Extensions;
 using Ordering.Application;
 using Ordering.infrastructure;
@@ -6,6 +9,9 @@ using Ordering.infrastructure.Persistence;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 
@@ -16,15 +22,34 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
 builder.Services.AddinfrastructureServices(builder.Configuration);
 
+////MassTransit-RabitMq Configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+            config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
+        });
+    });
+});
+
+
+///genereal configuration
+builder.Services.AddAutoMapper(typeof(Program));
+//builder.Services.AddScoped<BasketCheckoutConsumer>();
+
 var app = builder.Build();
 app.MigrationDataBase<OrderContext>((context, service) =>
 {
     var logger = service.GetService<ILogger<OrderContextSeed>>();
 
-    // ?? ????? ??????? ?? migrate ??
+ 
     context.Database.Migrate();
 
-    // ?? ???? ?? ???? ???? seed ??
+ 
     OrderContextSeed.SeedAsync(context, logger).Wait();
 });
 
